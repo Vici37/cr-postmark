3db91f7d78863744a82d24eb7ad84eff482e7bb7
Troy Sornson <troy@sornson.io>
2022-04-11 10:50:20 -0600

    Add spec for template email sending

--- spec/server_client_spec.cr
+++ spec/server_client_spec.cr
@@ -1,9 +1,5 @@
 require "./spec_helper"
 
-Spectator.configure do |config|
-  config.before_each { WebMock.reset }
-end
-
 Spectator.describe Postmark::ServerClient do
   it "sends email" do
     WebMock.stub(:post, "https://api.postmarkapp.com/email")
@@ -12,6 +8,17 @@ Spectator.describe Postmark::ServerClient do
 
     client = Postmark::ServerClient.new("token")
     resp = client.deliver_email(to: "you", from: "me", text_body: "nope")
+    expect(resp.to).to eq "you"
+    expect(resp.message).to eq "success"
+  end
+
+  it "sends template email" do
+    WebMock.stub(:post, "https://api.postmarkapp.com/email/withTemplate")
+      .with(headers: {"X-Postmark-Server-Token" => "token"})
+      .to_return(status: 200, body: %q({"To":"you","SubmittedAt":"now","MessageID":"1","ErrorCode":0,"Message":"success"}))
+
+    client = Postmark::ServerClient.new("token")
+    resp = client.deliver_template(to: "you", from: "me", template_model: {"success" => "yes"}, template_alias: "alias")
     expect(resp.to).to eq "you"
     expect(resp.message).to eq "success"
   end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,3 +1,7 @@
 require "spectator"
 require "webmock"
 require "../src/cr-postmark"
+
+Spectator.configure do |config|
+  config.before_each { WebMock.reset }
+end
